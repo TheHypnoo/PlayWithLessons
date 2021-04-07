5328fb9dddfc47a830c95d71b42ca3198f99d739
TheHypnoo <mysterlegendz@gmail.com>
2021-04-07 11:01:07 +0200

    User added photo_url and Created Logic Register.

--- app/src/main/java/com/upitnik/playwithlessons/data/model/auth/User.kt
+++ app/src/main/java/com/upitnik/playwithlessons/data/model/auth/User.kt
@@ -1,3 +1,3 @@
 package com.upitnik.playwithlessons.data.model.auth
 
-data class User(val email: String = "", val username: String = "")
+data class User(val email: String = "", val username: String = "", val photo_url: String = "")
--- app/src/main/java/com/upitnik/playwithlessons/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/upitnik/playwithlessons/ui/auth/RegisterFragment.kt
@@ -1,13 +1,111 @@
 package com.upitnik.playwithlessons.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.upitnik.playwithlessons.R
+import com.upitnik.playwithlessons.core.Result
+import com.upitnik.playwithlessons.data.remote.auth.AuthDataSource
+import com.upitnik.playwithlessons.databinding.FragmentRegisterBinding
+import com.upitnik.playwithlessons.domain.auth.AuthRepoImpl
+import com.upitnik.playwithlessons.presentation.auth.AuthViewModel
+import com.upitnik.playwithlessons.presentation.auth.AuthViewModelFactory
 
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
+    private lateinit var binding: FragmentRegisterBinding
+    private val viewModel by viewModels<AuthViewModel> {
+        AuthViewModelFactory(
+            AuthRepoImpl(
+                AuthDataSource()
+            )
+        )
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentRegisterBinding.bind(view)
+        SignUp()
     }
+
+
+    private fun SignUp() {
+
+        binding.btnSignUp.setOnClickListener {
+            val username = binding.editTextUsername.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+            val confirmPassword = binding.editTextConfirmPassword.text.toString().trim()
+            val email = binding.editTextEmail.text.toString().trim()
+
+            if (validateUserData(
+                    password,
+                    confirmPassword,
+                    username,
+                    email
+                )
+            ) return@setOnClickListener
+
+            createUser(email, password, username)
+        }
+    }
+
+    private fun createUser(email: String, password: String, username: String) {
+        viewModel.signUp(email, password, username).observe(viewLifecycleOwner, Observer { result ->
+            when (result) {
+                is Result.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.btnSignUp.isEnabled = false
+                }
+                is Result.Sucess -> {
+                    binding.progressBar.visibility = View.GONE
+                    findNavController().navigate(R.id.action_registerFragment_to_mainMenu)
+
+                }
+                is Result.Failure -> {
+                    binding.progressBar.visibility = View.GONE
+                    binding.btnSignUp.isEnabled = true
+                    Toast.makeText(
+                        requireContext(),
+                        "Error: ${result.exception}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        })
+    }
+
+    private fun validateUserData(
+        password: String,
+        confirmPassword: String,
+        username: String,
+        email: String
+    ): Boolean {
+        if (password != confirmPassword) {
+            binding.editTextConfirmPassword.error = "Las contraseñas no coinciden"
+            binding.editTextPassword.error = "Las contraseñas no coinciden"
+            return true
+        }
+        if (username.isEmpty()) {
+            binding.editTextUsername.error = "El usuario esta vacio"
+            return true
+        }
+        if (email.isEmpty()) {
+            binding.editTextEmail.error = "El correo esta vacio"
+            return true
+        }
+        if (password.isEmpty()) {
+            binding.editTextPassword.error = "La contraseña esta vacia"
+            return true
+        }
+        if (confirmPassword.isEmpty()) {
+            binding.editTextConfirmPassword.error = "Confirmar contraseña esta vacio"
+            return true
+        }
+        return false
+    }
+
 }
