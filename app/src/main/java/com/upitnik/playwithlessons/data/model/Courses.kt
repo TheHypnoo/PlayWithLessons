b4eecd23901ef0c9ff7f865f858fc8d198d5b304
TheHypnoo <mysterlegendz@gmail.com>
2021-04-08 19:16:29 +0200

    Added Retrofit and try testing Course in API.

---
+++ app/src/main/java/com/upitnik/playwithlessons/data/model/Courses.kt
@@ -0,0 +1,6 @@
+package com.upitnik.playwithlessons.data.model
+
+data class Courses(
+    val id: Int,
+    val name: String
+)
--- app/src/main/java/com/upitnik/playwithlessons/ui/MainMenu.kt
+++ app/src/main/java/com/upitnik/playwithlessons/ui/MainMenu.kt
@@ -1,15 +1,61 @@
 package com.upitnik.playwithlessons.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.upitnik.playwithlessons.R
+import com.upitnik.playwithlessons.data.model.Courses
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainMenu : Fragment(R.layout.fragment_main_menu) {
+    val BASEAPI = "http://10.0.2.2:8000/"
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASEAPI)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val httpService = retrofit.create(service::class.java)
 
+        val call: Call<Courses> =
+            httpService.getJSON("$BASEAPI/api/pwlassignatura")
+
+        call.enqueue(object : Callback<Courses> {
+
+            override fun onFailure(call: Call<Courses>, t: Throwable) {
+                t.message?.let {
+                    println(it)
+
+                }
+
+
+            }
+
+
+            override fun onResponse(call: Call<Courses>?, response: Response<Courses>?) {
+
+                if (!response!!.isSuccessful) {
+                    println(response.code())
+
+                    return
+
+                }
+
+                val cityList = response.body()
+
+                if (cityList != null) {
+                    println("ID: ${cityList.id} Name: ${cityList.name}")
+
+
+                }
+            }
+        })
 
     }
+
 }
